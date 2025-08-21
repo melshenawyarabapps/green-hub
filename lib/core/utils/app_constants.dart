1629ee9f9f7d44bf63200ef1cc1710e2b26444ba
melshenawyarabapps <114089659+melshenawyarabapps@users.noreply.github.com>
2025-08-21 21:00:47 +0300

    Update app version to 1.0.1
    This commit updates the application version from 1.0.0 to 1.0.1 in `app_constants.dart` and the build version from 1.0.0+1 to 1.0.1+2 in `pubspec.yaml`.

--- lib/core/utils/app_constants.dart
+++ lib/core/utils/app_constants.dart
@@ -14,7 +14,7 @@ class AppConstants {
 
   static AppConstants get instance => _instance ??= AppConstants._();
 
-  final String appVersion = '1.0.0';
+  final String appVersion = '1.0.1';
 
   String get androidStoreUrl => 'https://play.google.com/store/apps/details?id=com.saudi.goldprice';
 
