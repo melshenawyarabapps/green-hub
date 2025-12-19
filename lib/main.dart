4e965042649586af65426821ef936c59db07d878
melshenawy <114089659+melshenawyarabapps@users.noreply.github.com>
2025-12-20 00:22:34 +0200

    Rename main_user.dart to main.dart and add DeliveryApp
    Renamed lib/main_user.dart to lib/main.dart. Updated main entry to conditionally launch UserApp or DeliveryApp based on appFlavor. Added necessary imports for DeliveryApp and string extensions.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:greenhub/apps/delivery_app.dart';
 import 'package:greenhub/apps/user_app.dart';
 import 'package:greenhub/core/config/app_config.dart';
+import 'package:greenhub/core/extensions/string_extensions.dart';
 import 'package:greenhub/core/translations/codegen_loader.g.dart';
 
 Future<void> main() async {
@@ -15,7 +16,7 @@ Future<void> main() async {
       assetLoader: const CodegenLoader(),
       fallbackLocale: const Locale('ar'),
       startLocale: const Locale('ar'),
-      child: UserApp(),
+      child: appFlavor.isUser ? UserApp() : DeliveryApp(),
     ),
   );
 }
